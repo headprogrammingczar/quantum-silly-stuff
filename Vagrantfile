67e34760b3b959faeb3fcdd7f46b3e47030f6440
headprogrammingczar <headprogrammingczar@gmail.com>
2019-08-03 09:00:02 -0400

    secnd!

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+provision = "
+apt install -y cabal-install vim ghc
+sudo -u vagrant cabal update
+sudo -u vagrant cabal install QIO
+"
+
+Vagrant.configure("2") do |config|
+  config.vm.define "d" do |node|
+    node.vm.box = "debian/stretch64"
+    config.vm.synced_folder ".", "/vagrant", type: "sshfs"
+    node.vm.provision "shell", inline: provision
+    node.vm.provider "virtualbox" do |v|
+      v.memory = 2048
+    end
+  end
+end
---
+++ test.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE StandaloneDeriving, FlexibleInstances #-}
+import QIO.QExamples
+import QIO.QArith
+import QIO.Shor
+import QIO.Qio
+import QIO.QioSyn
+import QIO.QIORandom
+import QIO.Vec
+import Control.Monad
+import Data.List
+
+deriving instance Eq a => Eq (Vec RR a)
+deriving instance Ord a => Ord (Vec RR a)
+
+deriving instance Eq a => Eq (Prob a)
+deriving instance Ord a => Ord (Prob a)
+
+main = do
+  factors <- forM [1..100] $ \n -> do
+    let v = sim $ do
+              bit1 <- mkQbit False
+              bit2 <- mkQbit False
+              applyU (hadamards [bit1])
+              applyU (cnot bit1 bit2)
+              b1 <- measQbit bit1
+              b2 <- measQbit bit1
+              pure (b1, b2)
+    pure v
+  let factors' = [factors] --group (sort factors)
+  let factors'' = map (\fs -> (head fs, length fs)) factors'
+  print factors''
